24a2dae8c02c4e85dffcac8f548820e67c145a74
Ronny Alvarado <rsalvarado777@gmail.com>
2019-11-14 18:26:58 -0600

    Configured .env files and knexconfiguration

--- knexfile.js
+++ knexfile.js
@@ -32,18 +32,12 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
+      directory: "./data/migrations",
       tableName: "knex_migrations"
-    }
+    },
+    seeds: { directory: "./data/seeds" }
   }
 };
